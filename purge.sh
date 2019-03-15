92ce258cf369b2fd175a0f582907035fbdd14cd7
Bharat Pareek <bharat.pareek@wisdmlabs.com>
2019-03-15 13:42:09 +0530

    removed purge cache shell script, added by mistake

--- purge.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env sh
-PHP=`which php`
-php /var/www/html/moodle34/admin/cli/purge_caches.php
-php /var/www/html/moodle35/admin/cli/purge_caches.php
-php /var/www/html/moodle36/admin/cli/purge_caches.php
