cf05201aec1a9a957c51c186842574e826f11ab5
Ranjit <ranjitlawale11@gmail.com>
2020-06-18 15:19:10 +0530

    version.php Updated - v-1.0.7

--- version.php
+++ version.php
@@ -24,8 +24,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2020051400;                    // The current plugin version (Date: YYYYMMDDXX).
-$plugin->release   = '1.0.6';
+$plugin->version   = 2020061800;                    // The current plugin version (Date: YYYYMMDDXX).
+$plugin->release   = '1.0.7';
 $plugin->requires  = 2017111300;                    // Requires this Moodle version (Moodle V3.4.0).
 $plugin->maturity  = MATURITY_STABLE;
 $plugin->component = 'format_remuiformat';          // Full name of the plugin (used for diagnostics).
