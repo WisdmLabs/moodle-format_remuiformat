fbf6ae8fb5193123d827e05a435ce155220a7ee8
Yogesh <yogesh.shirsath@wisdmlabs.com>
2020-09-02 12:06:19 +0530

    Updated version.php for v1.0.8

--- version.php
+++ version.php
@@ -24,8 +24,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2020061801;                    // The current plugin version (Date: YYYYMMDDXX).
-$plugin->release   = '1.0.7';
+$plugin->version   = 2020090200;                    // The current plugin version (Date: YYYYMMDDXX).
+$plugin->release   = '1.0.8';
 $plugin->requires  = 2017111300;                    // Requires this Moodle version (Moodle V3.4.0).
 $plugin->maturity  = MATURITY_STABLE;
 $plugin->component = 'format_remuiformat';          // Full name of the plugin (used for diagnostics).
