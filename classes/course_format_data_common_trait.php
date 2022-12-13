f159280e3f8a94fb043b950ad40f64d7b949d655
abhishek.kushwah <abhishek.kushwah@wisdmlabs.com>
2022-12-13 18:36:23 +0530

    gradient value of cards updated

--- classes/course_format_data_common_trait.php
+++ classes/course_format_data_common_trait.php
@@ -345,7 +345,7 @@ class course_format_data_common_trait {
                     // } else if ( $remuidefaultsectiontheme == 1 &&  $imgarray['pattern'] == 1 ) {
                     // Dark theme.
                     // $remuidefaultsectionoverlay = 'rgba(0, 0, 0, 0.55) ';
-                        $remuidefaultsectionoverlay = "179.62deg, rgba(0, 0, 0, 0) 6.42%, #000000b5 71.44%";
+                        $remuidefaultsectionoverlay = "180deg, rgba(255, 255, 255, 0) 0%, #000000 110%";
                     // }
                     $data->remuidefaultsectionmode = $remuidefaultsectionmode;
                     $data->remuidefaultsectionoverlay = $remuidefaultsectionoverlay;
