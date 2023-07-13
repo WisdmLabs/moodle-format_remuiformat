37a568ad4d4ae3ed949374727290be8492f996e8
Abhishek Kushwah <abhishek.kushwah@wisdmlabs.com>
2023-07-13 18:40:36 +0530

    section restriction added, next previous button issue

--- version.php
+++ version.php
@@ -23,7 +23,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2023053000;                    // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version   = 2023053003;                    // The current plugin version (Date: YYYYMMDDXX).
 $plugin->release   = '4.1.3';
 $plugin->requires  = 2022041900;                    // Requires this Moodle version (Moodle V4.0).
 $plugin->maturity  = MATURITY_STABLE;
