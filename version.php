96c6527f593b893ec40ed4376277783f68738f18
Abhishek Kushwah <abhishek.kushwah@wisdmlabs.com>
2023-10-17 12:14:53 +0530

    Updating version [4.1.4] and changes.txt file

--- version.php
+++ version.php
@@ -23,8 +23,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2023053003;                    // The current plugin version (Date: YYYYMMDDXX).
-$plugin->release   = '4.1.3';
+$plugin->version   = 2023101700;                    // The current plugin version (Date: YYYYMMDDXX).
+$plugin->release   = '4.1.4';
 $plugin->requires  = 2022041900;                    // Requires this Moodle version (Moodle V4.0).
 $plugin->maturity  = MATURITY_STABLE;
 $plugin->component = 'format_remuiformat';          // Full name of the plugin (used for diagnostics).
