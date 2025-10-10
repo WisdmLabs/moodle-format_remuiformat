caa7040db422712ff56dc2880e7431ff8b5c9b10
JasonWisdmLabs <jason.dsa@wisdmlabs.com>
2025-10-10 14:49:38 +0530

    Fixed course_section_add_cm_control deprecation warning

--- classes/output/card_one_section_renderable.php
+++ classes/output/card_one_section_renderable.php
@@ -198,11 +198,19 @@ class format_remuiformat_card_one_section implements renderable, templatable {
         // Get the details of the activities.
         $export->activities = $this->get_activities_details($currentsection);
         $export->courseid = $this->course->id;
-        $export->addnewactivity = $this->courserenderer->course_section_add_cm_control(
-            $this->course,
-            $this->displaysection,
-            $this->displaysection
-        );
+        if($CFG->branch >= '501'){
+            $export->addnewactivity = $this->courserenderer->section_add_cm_controls(
+                $this->course,
+                $this->displaysection
+            );
+            
+        }else{
+            $export->addnewactivity = $this->courserenderer->course_section_add_cm_control(
+                $this->course,
+                $this->displaysection,
+                $this->displaysection
+            );
+        }
         $export->remuicourseformatcard = true;
         $export->sections = [];
         foreach ($sections as $index => $sectioninfo) {
